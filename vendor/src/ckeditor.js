ba9256547938090abad9bc124c29504fe409520e
Ricardo <ricardo-verdugo@hotmail.com>
2019-08-15 01:41:59 +0100

    fix lint error

--- vendor/src/ckeditor.js
+++ vendor/src/ckeditor.js
@@ -7,7 +7,7 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-//import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+// import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
@@ -15,20 +15,20 @@ import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 // import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 // import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-//import Image from '@ckeditor/ckeditor5-image/src/image';
-//import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-//import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-//import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-//import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+// import Image from '@ckeditor/ckeditor5-image/src/image';
+// import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+// import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+// import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+// import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-//import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-//import Table from '@ckeditor/ckeditor5-table/src/table';
-//import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+// import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+// import Table from '@ckeditor/ckeditor5-table/src/table';
+// import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 
-//devtool
+// devtool
 // import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
 
 // Custom Plugins go below here
